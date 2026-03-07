8213eac62287bfc6dc68455bcfa1b3fd934f0259
Robert <144620265+Robertlyhh@users.noreply.github.com>
2026-03-07 16:11:24 -0500

    use WANDB_PROJECT for chat_sft/chat_rl logging

--- scripts/chat_sft.py
+++ scripts/chat_sft.py
@@ -87,7 +87,12 @@ else:
 
 # wandb logging init
 use_dummy_wandb = args.run == "dummy" or not master_process
-wandb_run = DummyWandb() if use_dummy_wandb else wandb.init(project="nanochat-sft", name=args.run, config=user_config)
+wandb_project = os.environ.get("WANDB_PROJECT", "490-autobook-a4")
+wandb_run = (
+    DummyWandb()
+    if use_dummy_wandb
+    else wandb.init(project=wandb_project, name=args.run, config=user_config)
+)
 
 # Flash Attention status
 if not HAS_FA3:
